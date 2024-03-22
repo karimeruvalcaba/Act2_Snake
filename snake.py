271adef25efff37b77155daa71ce381fe68e81be
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2024-03-22 14:18:16 -0600

    Update snake.py
    adding the colors

--- snake.py
+++ snake.py
@@ -1,31 +1,31 @@
-#Se mueve one square per time
-from random import randrange
+#complete code
+from random import randrange, choice
 from turtle import *
-
 from freegames import square, vector
+
+colors = ['blue', 'green', 'yellow', 'purple', 'orange']
+
+food_color = choice([c for c in colors if c != 'red'])
+snake_color = choice([c for c in colors if c != food_color and c != 'red'])
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-
 
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
 
-
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
-    return -200 < food.x < 190 and -200 < food.y < 190
-
-
 
 def move():
     """Move snake forward one segment."""
-    if randrange (10) == 0:
-       food.x += 1
+    if randrange(10) == 0:
+        food.x += 1
+
     head = snake[-1].copy()
     head.move(aim)
 
@@ -46,16 +46,16 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, snake_color)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, food_color)
     update()
 
-        #avanza food 
-    if randrange (10) == 0:
-       food.x += 1
-    ontimer(move, 100)
+    # Advance food
+    if randrange(10) == 0:
+        food.x += 1
 
+    ontimer(move, 100)
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -66,4 +66,4 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
-done(
+done()
